1906ed8734acb4f48b5c30fdea44ae6aaf1fbc24
Ziqun Hsu <kaspay0722@gmail.com>
2022-10-11 09:02:34 +0800

    updated

--- Week 5/Homework/1.c
+++ Week 5/Homework/1.c
@@ -1,35 +1,46 @@
-//Dev environment: Dev-C++ 5.11
 //B101020021 徐子群 
+//開發環境: Dev-C++ 5.11 
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-	float x, y;
-	printf("Please enter X cordinate:");
-	scanf("%f",&x);
-	printf("Please enter Y cordinate:");
-	scanf("%f",&y);
+    int number, i, j, k, l;
+    char primeone, primetwo;
+    
+    printf("請輸入大於二的偶數:");
+    scanf("%d",&number);
+    
+    if(number%2==0, number>2)	//If number is an even and >2.
+	{
+    	for (i = 3; i < number/2; i += 2)	//開始判斷第一個質數i. 
+    		{
+        	primeone = 1;
+        	
+        	for (k = 2; k < sqrt(i); k++)	//判斷k(範圍從2~至根號i)能否整除i. 
+            	if (i % k == 0)	//如果能整除, 說明i非質數 
+               		primeone = 0;	//因此把primeone設為0 
+        	if (primeone)	//倘若primeone非零(則為1, 也就是i為質數的情況) 
+        		{
+        		j = number - i;	//設另一數j必須要等於number-i 
+				primetwo = 1;
+				
+                	for (l = 2; l < j; l++)	 
+                    	if (j % l == 0)	//若j非質數之情況 
+                        	primetwo = 0;
+                	if (primetwo)	//若j為質數, 且i+j=number 
+                    	{
+                        	printf("\n%d = %d + %d", number, i, j);
+                        	return 0;
+                    	}
+            	}
+        	}
+    }
 	
-	//First quadrant(x>0, y>0)
-	if(x>0 && y>0)
-	printf("point(%f,%f) is in the first quadrant",x, y);
-	
-	//Second quadrant(x<0, y>0)
-	else if(x<0 && y>0)
-	printf("point(%f,%f) is in the second quadrant",x, y);
-	
-	//Third quadrant(x<0, y<0)
-	else if(x<0 && y<0)
-	printf("point(%f,%f) is in the third quadrant",x, y);
-	
-	//Fourth quadrant(x>0, y<0)
-	else if(x>0 && y<0)
-	printf("point(%f,%f) is in the fourth quadrant",x, y);
-	
-	//Once if x, or y = zero occurs
-	else
-	printf("point(%f,%f) is not in any quadrant",x, y);
+	else	//當使用者輸入大於二的偶數以外的東西時便跳出 
+	printf("請確認輸入的值為大於二的偶數, 再試一次");
 	
 	return 0;
 }
--- Week 5/Homework/2.c
+++ Week 5/Homework/2.c
@@ -1,63 +1,40 @@
-//Dev environment: Dev-C++ 5.11
 //B101020021 徐子群 
+//開發環境: Dev-C++ 5.11 
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-	int sweet;
-	char size;
-	printf("Please enter size:");
-	scanf("%c", &size);
-	printf("Please enter sweetness:");
-	scanf("%d", &sweet);
+	int number, digit, sum=0, digit2, sum2=0;
 	
-	switch(size){
-	case 'L': case 'l': case 'E': case 'e': case 'M': case 'm': case 'S': case 's':
-		
-	//The code block below only runs when the size is under correct value
+	printf("請輸入一正整數:");
+	scanf("%d", &number);
 	
+	if(number<=2*pow(10, 9) && number>0)	//限定輸入數字須為正整數,且小於2x10^9 
+	{
+		while(number>0)	
 		{
-	if(sweet >= 85 && sweet <=100){
-		switch(size){
-			case 'L': case 'l': case 'E': case 'e':
-				printf("The fruit is Premium class");
-				break;
-			case 'M': case 'm':
-				printf("The fruit is Gifted class");
-				break;
-			case 'S': case 's':
-				printf("The fruit is Regular class");
-				break;
+			digit = number % 10;	//由%10來取個位數的值 
+			sum += digit;	//將個位數的值加總 
+			number /= 10;	//在取完個位數的值後, 將輸入數字除以十(將十位移至個位, 直至除到0為止)[^1]
 		}
-	}
-	else if(sweet >= 60 && sweet < 85){
-		switch(size){
-			case 'L': case 'l': case 'E': case 'e': case 'M': case 'm':
-				printf("The fruit is Gifted class");
-				break;
-				case 'S': case 's':
-				printf("The fruit is Regular class");
-				break;	
+		if(sum>9)	//判斷while迴圈結束後, 加總值sum是否>9(大於一位數) 
+		{
+			while(sum>0)	 
+			{
+			digit2 = sum % 10;	//取sum的個位數值 
+			sum2 += digit2;	//將sum的個位數值加總 
+			sum /=10;	//將sum的十位數移至個位, 直至sum=0, 迴圈結束 
+			}
+			printf("%d", sum2);	//若sum>9, 則輸出總和為sum2 
+			return 0;
 		}
+		printf("%d", sum);	//若sum僅為一個位數, 則輸出sum本身 
 	}
-	else if(sweet >= 40 && sweet < 60)
-	printf("The fruit is Regular class");
 	
-	else if(sweet >= 1 && sweet < 40)
-	printf("The fruit is Inferior class");
-	
-	//The size is correct, but the value for sweetness' not in range
-	 
-	else
-	printf("Wrong Value for Sweetness!");
-		}
-		break;
-	
-	//If the size of fruit is wrong, then tells the users that it's not a value in range 
-	
-	default:
-	printf("Wrong Fruit Size!");
-	}
-return 0;
+	else	//當輸入值>2x10^9 或<=0時呈現 
+	printf("輸入的值超出範圍! 請再試一次");
+	return 0;
 }
